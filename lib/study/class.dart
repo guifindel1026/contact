be11e01f1df97a74070df86cf158867e9c5f9f17
guifindel <guifindel@gmail.com>
2025-05-20 18:03:13 +0900

    20250520

--- lib/study/class.dart
+++ lib/study/class.dart
@@ -1,26 +1,18 @@
 void main() {
-  Idol blackPink = const Idol('블랙핑크', ['지수', '제니', '리사', '로제']);
+  _Idol blackPink = _Idol('블랙핑크', ['지수', '제니', '리사', '로제']);
 
-  Idol blackPink2 = const Idol('블랙핑크', ['지수', '제니', '리사', '로제']);
-
-  print(blackPink.name);
-  print(blackPink.members);
-  blackPink.sayHello();
-  blackPink.introduce();
-
-  print(blackPink == blackPink2);
-
-  // Idol bts = Idol('BTS', ['진', '슈가', '제이홉', '지민', '뷔', '정국']);
-
-  Idol bts = Idol.fromList([
+  _Idol bts = _Idol.fromList([
     ['진', '슈가', '제이홉', '지민', '뷔', '정국'],
     'BTS',
   ]);
 
-  print(bts.name);
-  print(bts.members);
-  bts.sayHello();
-  bts.introduce();
+  print(blackPink.firstMember);
+  print(bts.firstMember);
+
+  blackPink.firstMember = '아린';
+  bts.firstMember = '구스타브';
+  print(blackPink.firstMember);
+  print(bts.firstMember);
 }
 
 // idol class
@@ -31,7 +23,10 @@ void main() {
 //
 // constructor (생성자)
 // immutable 프로그래밍
-class Idol {
+
+// getter / setter
+// 데이터를 가져올때 / 데이터를 설정할때때
+class _Idol {
   final String name;
   final List<String> members;
 
@@ -39,9 +34,9 @@ class Idol {
   //   : this.name = name,
   //     this.members = members;
 
-  const Idol(this.name, this.members);
+  _Idol(this.name, this.members);
 
-  Idol.fromList(List values) : this.members = values[0], this.name = values[1];
+  _Idol.fromList(List values) : this.members = values[0], this.name = values[1];
 
   void sayHello() {
     print('안녕하세요. ${this.name}입니다.');
@@ -49,5 +44,15 @@ class Idol {
 
   void introduce() {
     print('저희 멤버는 ${this.members}가 있습니다.');
+  }
+
+  // getter
+  String get firstMember {
+    return this.members[0];
+  }
+
+  // setter
+  set firstMember(String member) {
+    this.members[0] = member;
   }
 }
